7bbfbf2a534f9863ae0a32d2c9d0a5c577002acf
PriyalMangal50 <145255577+PriyalMangal50@users.noreply.github.com>
2025-01-24 00:36:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,8 +6,12 @@ import UserRoutes from "./routes/User.js";
 
 dotenv.config();
 
-const app = express();
-app.use(cors());
+const app = express();const corsOptions = {
+  origin: "https://fitness-track-two.vercel.app", // Allow only your frontend URL
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
